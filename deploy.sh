08c9259f7a0d109efb152d288fd580712eda6efc
brett ohland <brett@ohland.ca>
2023-11-25 09:02:10 -0700

    Removing Keybase proofs

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 hugo 
 rm -rf public/_includes
-cp keybase/${FCK}/keybase.txt public/keybase.txt
 rsync -avz --delete public/ ${USER}@${HOST}:~/${FCK}
 hugo --config config-gosh.toml 
 rm -rf public/_includes
-cp keybase/${GOSH}/keybase.txt public/keybase.txt
 rsync -avz --delete public/ ${USER}@${HOST}:~/${GOSH}
 
 exit 0
